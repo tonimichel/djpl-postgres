3a10a60bec0f52845b234550edcded89b0bbc35c
tonimichel <toni@schnapptack.de>
2013-09-28 17:12:11 +0200

    adjusted default values

--- postgres/settings.py
+++ postgres/settings.py
@@ -2,10 +2,10 @@ def refine_DATABASES(original):
     return {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-            'HOST': '{{ pg_host }}', 
-            'PASSWORD': '{{ pg_password }}', 
-            'NAME': '{{ pg_name }}', 
-            'USER': '{{ pg_username }}'
+            'HOST': None, 
+            'PASSWORD': None, 
+            'NAME': None, 
+            'USER': None
         }
     }
 
