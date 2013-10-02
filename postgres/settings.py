64baf091ff129f1183e01249ac729e742beb4d59
tonimichel <toni@schnapptack.de>
2013-10-02 09:47:00 +0200

    raise Exception if context.json does not provide database keys

--- postgres/settings.py
+++ postgres/settings.py
@@ -1,4 +1,11 @@
-
-# set DATABASES from PRODUCT_CONTEXT
 from django_productline.context import PRODUCT_CONTEXT
-refine_DATABASES = PRODUCT_CONTEXT.DATABASES
+
+
+if hasattr(PRODUCT_CONTEXT, 'DATABASES'):
+    # set DATABASES from PRODUCT_CONTEXT
+    refine_DATABASES = PRODUCT_CONTEXT.DATABASES
+else:
+    raise AttributeError('''
+        postgres feature is selected but configuration is missing in PRODUCT_CONTEXT.
+        Did you run "ape generate_context"?
+    ''')
