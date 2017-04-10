c3521a89c057a86a348dfe5b7e139541209b86c6
Michael Galler <michael@schnapptack.de>
2017-04-10 12:23:54 +0200

    on ape manage test do not enable postgres

--- postgres/settings.py
+++ postgres/settings.py
@@ -3,7 +3,7 @@ def refine_DATABASES(orginal):
     import sys
     from django_productline.context import PRODUCT_CONTEXT
     # return orginal(sqlite) if this is a test run
-    if 'run_product_tests' in sys.argv:
+    if 'run_product_tests' in sys.argv or 'test' in sys.argv:
         return orginal
     # set DATABASES from PRODUCT_CONTEXT
     return {
